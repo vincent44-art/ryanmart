0717559489914e30cf2958539115cf2ca57957b4
vincent44-art <vincent4431murithi@gmail.com>
2026-04-21 12:42:46 +0300

    kj

--- backend/utils/pdf_generator.py
+++ backend/utils/pdf_generator.py
@@ -1,7 +1,10 @@
-# Stub PDF generator for car expenses reports
-# Full implementation can be added later
+"""
+Stub PDF Generator for Driver Expenses.
+Returns minimal valid PDF bytes.
+"""
 
 class DriverExpensePDFGenerator:
-    def generate_daily_report(self, expense_data, driver_email, report_date):
-        # Stub: return empty PDF bytes
-        return b'%PDF-1.4\n1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n4 0 obj\n<< /Length 44 >>\nstream\nBT /F1 24 Tf 100 700 Td (Driver Expense Report) Tj ET\nendstream\nendobj\n5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\nxref\n0 6\n0000000000 655
+    def __init__(self):
+        pass
+
+    def generate_daily
